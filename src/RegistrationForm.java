7cef1756936f411c5c3c7e021a8c44d586a3f7f9
Ying Ying Feng <yingyingfengg@gmail.com>
2023-02-25 18:57:48 -0500

    Adding an action listener and new method for registering a user.

--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RegistrationForm extends JDialog {
     private JTextField tfName;
@@ -20,6 +22,19 @@ public class RegistrationForm extends JDialog {
         setModal(true);
         setLocationRelativeTo(parent);
         setVisible(true);
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //click on button, create listener, action listener, ok
+                //^this method will be executed when we click on register button.
+                //^ when clicking, we want to register a new user
+                registerUser();
+                //^ can be created by on the left, crete method, and click the bottom option, then it is created underneath
+            }
+        });
+    }
+
+    private void registerUser() {
     }
 
     public static void main(String[] args) {
