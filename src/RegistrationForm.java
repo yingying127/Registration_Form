4569216399fe16da76c50b258956cd8f0373de44
Ying Ying Feng <yingyingfengg@gmail.com>
2023-02-25 19:22:54 -0500

    connecting to database w mysql and localhost

--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class RegistrationForm extends JDialog {
     private JTextField tfName;
@@ -92,11 +96,58 @@ public class RegistrationForm extends JDialog {
     //this method will return the user object, if method does succeed in creating a new method
     private User addUserToDatabase(String name, String email, String phone, String address, String password) {
         User user = null; //fail = return null
+
+        //define variables to connect to database:
+        final String DB_URL = "jdbc:mysql://localhost/MyStore?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        //connect to database:
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+                //Connected to database successfully..
+                //create an sql statement to add a new user:
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO users (name, email, phone, address, password) " + "VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+                //values added to the sql:
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, password);
+
+            //execute the sql: by inserting rows into the table
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0) {
+                user = new User();
+                user.name = name;
+                user.email = email;
+                user.phone = phone;
+                user.address = address;
+                user.password = password;
+            }
+
+            //close the connection:
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
         return user; //success = valid user
     }
 
     public static void main(String[] args) {
         RegistrationForm myForm = new RegistrationForm(null);
+        //Checking if the registration is completed:
+        User user = myForm.user;
+        if (user != null) {
+            System.out.println("Successful registration of: " + user.name);
+        } else {
+            System.out.println("Registration canceled.");
+        }
     }
 
     private void createUIComponents() {
