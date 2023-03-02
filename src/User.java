50be2b592218ffb7b43e72929a98dad0a77a3661
Ying Ying Feng <yingyingfengg@gmail.com>
2023-03-02 14:01:38 -0500

    commit

---
+++ src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    //adding some class variables:
+    //classes are public bc we want to access them outside this User.java class
+    public String name;
+    public String email;
+    public String phone;
+    public String address;
+    public String password;
+}
